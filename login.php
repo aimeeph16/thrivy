a0cdd630c5033b3950fae40358a6e98d70b136d1
aimeeph16 <55416105+aimeeph16@users.noreply.github.com>
2021-06-27 15:05:27 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'header.php';?>
+</head>
+    <body>
+        <div class="login">
+            <div class="login__content">
+                <div class="login__img">
+                    <img src="assets2/images/login-register.svg" alt="">
+                </div>
+
+                <div class="login__forms">
+                    <form action="" class="login__registre" id="login-in">
+                        <h1 class="login__title">Sign In</h1>
+    
+                        <div class="login__box">
+                            <i class='bx bx-user login__icon'></i>
+                            <input type="text" placeholder="Username" class="login__input">
+                        </div>
+    
+                        <div class="login__box">
+                            <i class='bx bx-lock-alt login__icon'></i>
+                            <input type="password" placeholder="Password" class="login__input">
+                        </div>
+
+                        <a href="#" class="login__forgot">Forgot password?</a>
+
+                        <a href="#" class="login__button">Sign In</a>
+
+                        <div>
+                            <span class="login__account">Don't have an Account ?</span>
+                            <a href="register.html" class="login__signin" id="sign-up">Sign Up</span>
+                        </div>
+                    </form>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!--===== MAIN JS =====
+        <script src="login.js"></script>-->
+    </body>
+</html>
