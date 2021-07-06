53cb3aa0bde2d9d1a67185cd4354e3501c9915ec
Callista Roselynn <c4lly.ros3lynn@gmail.com>
2021-07-06 23:17:32 +0700

    commit

--- components/partials/FrontFooter.vue
+++ components/partials/FrontFooter.vue
@@ -3,8 +3,8 @@
     <div class="footer-bottom">
       <div class="container">
         <div class="row">
-          <p class="pull-left">Copyright © 2020 E-SHOPPER Inc. All rights reserved.</p>
-          <p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+          <p class="pull-left">Copyright © 2021 Thrivy. All rights reserved.</p>
+          <p class="pull-right">Designed by <span><a target="_blank" href="https://en.wikipedia.org/wiki/Human">Humans</a></span></p>
         </div>
       </div>
     </div>
